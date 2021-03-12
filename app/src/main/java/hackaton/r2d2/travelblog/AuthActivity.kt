666f164d4f579717e8078a499873e5935412cd93
volkovskiyda <volkovskiyda@gmail.com>
2021-03-12 21:43:20 +0200

    add maps and auth

--- app/src/main/java/hackaton/r2d2/travelblog/AuthActivity.kt
+++ app/src/main/java/hackaton/r2d2/travelblog/AuthActivity.kt
@@ -1,6 +1,7 @@
 package hackaton.r2d2.travelblog
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
@@ -8,29 +9,60 @@ import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import hackaton.r2d2.travelblog.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
 
-    val signIn = registerForActivityResult(StartActivityForResult()) { result ->
+    private lateinit var binding: ActivityAuthBinding
+    private val signIn = registerForActivityResult(StartActivityForResult()) { result ->
         val response = IdpResponse.fromResultIntent(result.data)
         if (result.resultCode == Activity.RESULT_OK) {
-            val user = FirebaseAuth.getInstance().currentUser
+            openUser(FirebaseAuth.getInstance().currentUser)
         } else {
-            Toast.makeText(this, response?.error?.message, Toast.LENGTH_LONG).show()
+            val errorMsg = response?.error?.message ?: "Failed to auth"
+            Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView()
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnEnter.setOnClickListener { auth() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        openUser(Firebase.auth.currentUser)
     }
 
     private fun auth() {
-        signIn.launch(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(listOf(AuthUI.IdpConfig.GoogleBuilder().build()))
-                .build()
-        )
+        if (binding.chbBlogger.isChecked) {
+            signIn.launch(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(listOf(AuthUI.IdpConfig.GoogleBuilder().build()))
+                    .build()
+            )
+        }
+        Firebase.auth.signInAnonymously().addOnCompleteListener { openConsumer() }
+    }
+
+    private fun openUser(user: FirebaseUser?) {
+        if (user != null) {
+            if (user.isAnonymous) openConsumer() else openBlogger()
+        }
+    }
+
+    private fun openConsumer() {
+        Toast.makeText(this, "openConsumer", Toast.LENGTH_SHORT).show()
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun openBlogger() {
+        Toast.makeText(this, "openBlogger", Toast.LENGTH_SHORT).show()
     }
 }
