e0bd7dc3fc1bd61c0781f6996b1c44a03c8cae27
volkovskiyda <volkovskiyda@gmail.com>
2021-03-12 19:44:32 +0200

    init

---
+++ app/src/test/java/hackaton/r2d2/travelblog/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package hackaton.r2d2.travelblog
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
