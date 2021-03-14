bb818294ddc2f696a7131486d798f3d2ad35c52b
volkovskiyda <volkovskiyda@gmail.com>
2021-03-14 12:52:46 +0200

    add sign out

--- app/src/main/java/hackaton/r2d2/travelblog/BloggerActivity.kt
+++ app/src/main/java/hackaton/r2d2/travelblog/BloggerActivity.kt
@@ -1,7 +1,11 @@
 package hackaton.r2d2.travelblog
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import hackaton.r2d2.travelblog.camera.CameraFragment
 import hackaton.r2d2.travelblog.databinding.ActivityBloggerBinding
 
@@ -17,5 +21,16 @@ class BloggerActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, CameraFragment())
                 .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.log_out, menu)
+        menu?.findItem(R.id.log_out)?.setOnMenuItemClickListener {
+            Firebase.auth.signOut()
+            startActivity(Intent(this, AuthActivity::class.java))
+            finish()
+            return@setOnMenuItemClickListener true
+        }
+        return super.onCreateOptionsMenu(menu)
     }
 }
